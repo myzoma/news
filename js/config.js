e15c01269f09d2b3ae8ededa0d124a12b221b56c
myzoma <86922707+myzoma@users.noreply.github.com>
2025-07-02 17:48:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,53 +1,44 @@
-// إعدادات التطبيق
 const CONFIG = {
-    // مصادر الأخبار (استخدام APIs مجانية)
-    NEWS_SOURCES: {
-        'CoinDesk': {
-            name: 'CoinDesk',
-            rss: 'https://feeds.feedburner.com/CoinDesk',
-            color: '#ff6b35'
-        },
-        'Cointelegraph': {
-            name: 'Cointelegraph', 
-            rss: 'https://cointelegraph.com/rss',
-            color: '#00d4ff'
-        },
-        'CryptoNews': {
-            name: 'CryptoNews',
-            rss: 'https://cryptonews.com/news/feed/',
-            color: '#f39c12'
-        },
-        'NewsBTC': {
-            name: 'NewsBTC',
-            rss: 'https://www.newsbtc.com/feed/',
-            color: '#27ae60'
-        },
-        'CoinGape': {
-            name: 'CoinGape',
-            rss: 'https://coingape.com/feed/',
-            color: '#9b59b6'
-        }
-    },
-    
-    // إعدادات عامة
-    ITEMS_PER_PAGE: 10,
-    AUTO_REFRESH_INTERVAL: 300000, // 5 دقائق
-    CACHE_DURATION: 600000, // 10 دقائق
-    
-    // RSS to JSON proxy services
     RSS_PROXY_SERVICES: [
         'https://api.rss2json.com/v1/api.json?rss_url=',
         'https://cors-anywhere.herokuapp.com/',
         'https://api.allorigins.win/get?url='
     ],
     
-    // CoinGecko API للأسعار (مجاني)
-    COINGECKO_API: 'https://api.coingecko.com/api/v3',
+    NEWS_SOURCES: {
+        coindesk: {
+            name: 'CoinDesk',
+            rss: 'https://www.coindesk.com/arc/outboundfeeds/rss/',
+            color: '#FF6B35'
+        },
+        cointelegraph: {
+            name: 'Cointelegraph',
+            rss: 'https://cointelegraph.com/rss',
+            color: '#1DA1F2'
+        },
+        bitcoinmagazine: {
+            name: 'Bitcoin Magazine',
+            rss: 'https://bitcoinmagazine.com/.rss/full/',
+            color: '#F7931A'
+        },
+        decrypt: {
+            name: 'Decrypt',
+            rss: 'https://decrypt.co/feed',
+            color: '#00D4AA'
+        },
+        theblock: {
+            name: 'The Block',
+            rss: 'https://www.theblockcrypto.com/rss.xml',
+            color: '#000000'
+        }
+    },
     
-    // إعدادات التخزين المحلي
     STORAGE_KEYS: {
-        NEWS_CACHE: 'crypto_news_cache',
-        LAST_UPDATE: 'last_update',
-        USER_PREFERENCES: 'user_preferences'
-    }
+        NEWS_CACHE: 'cryptoNewsCache',
+        LAST_UPDATE: 'lastUpdate'
+    },
+    
+    CACHE_DURATION: 5 * 60 * 1000, // 5 دقائق
+    
+    COINGECKO_API: 'https://api.coingecko.com/api/v3'
 };
