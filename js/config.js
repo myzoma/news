87c4f8848e93b39134d782631aaf3275ecde8abe
myzoma <86922707+myzoma@users.noreply.github.com>
2025-07-03 12:18:07 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,53 +1,66 @@
 // إعدادات التطبيق
 const CONFIG = {
-    // مصادر الأخبار (استخدام APIs مجانية)
+    // مصادر الأخبار
     NEWS_SOURCES: {
-        'CoinDesk': {
+        coindesk: {
             name: 'CoinDesk',
             rss: 'https://feeds.feedburner.com/CoinDesk',
+            color: '#f7931a'
+        },
+        cointelegraph: {
+            name: 'Cointelegraph',
+            rss: 'https://cointelegraph.com/rss',
+            color: '#1a73e8'
+        },
+        bitcoinmagazine: {
+            name: 'Bitcoin Magazine',
+            rss: 'https://bitcoinmagazine.com/feed',
             color: '#ff6b35'
         },
-        'Cointelegraph': {
-            name: 'Cointelegraph', 
-            rss: 'https://cointelegraph.com/rss',
-            color: '#00d4ff'
+        decrypt: {
+            name: 'Decrypt',
+            rss: 'https://decrypt.co/feed',
+            color: '#00d4aa'
         },
-        'CryptoNews': {
-            name: 'CryptoNews',
-            rss: 'https://cryptonews.com/news/feed/',
-            color: '#f39c12'
-        },
-        'NewsBTC': {
-            name: 'NewsBTC',
-            rss: 'https://www.newsbtc.com/feed/',
-            color: '#27ae60'
-        },
-        'CoinGape': {
-            name: 'CoinGape',
-            rss: 'https://coingape.com/feed/',
-            color: '#9b59b6'
+        theblock: {
+            name: 'The Block',
+            rss: 'https://www.theblockcrypto.com/rss.xml',
+            color: '#1e1e1e'
         }
     },
-    
-    // إعدادات عامة
-    ITEMS_PER_PAGE: 10,
-    AUTO_REFRESH_INTERVAL: 300000, // 5 دقائق
-    CACHE_DURATION: 600000, // 10 دقائق
-    
-    // RSS to JSON proxy services
+
+    // خدمات RSS Proxy
     RSS_PROXY_SERVICES: [
         'https://api.rss2json.com/v1/api.json?rss_url=',
-        'https://cors-anywhere.herokuapp.com/',
-        'https://api.allorigins.win/get?url='
+        'https://api.allorigins.win/get?url=',
+        'https://cors-anywhere.herokuapp.com/'
     ],
-    
-    // CoinGecko API للأسعار (مجاني)
+
+    // API العملات الرقمية
     COINGECKO_API: 'https://api.coingecko.com/api/v3',
-    
-    // إعدادات التخزين المحلي
+
+    // مفاتيح التخزين المحلي
     STORAGE_KEYS: {
         NEWS_CACHE: 'crypto_news_cache',
-        LAST_UPDATE: 'last_update',
-        USER_PREFERENCES: 'user_preferences'
+        LAST_UPDATE: 'crypto_news_last_update',
+        USER_PREFERENCES: 'crypto_news_preferences'
+    },
+
+    // مدة الكاش (بالميلي ثانية)
+    CACHE_DURATION: 15 * 60 * 1000, // 15 دقيقة
+
+    // إعدادات العرض
+    DISPLAY: {
+        NEWS_PER_PAGE: 10,
+        MAX_DESCRIPTION_LENGTH: 180,
+        THUMBNAIL_SIZE: {
+            DESKTOP: { width: 80, height: 80 },
+            MOBILE: { width: 50, height: 50 }
+        }
     }
 };
+
+// تصدير الإعدادات للاستخدام في ملفات أخرى
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
